1eed2da848acc10971a8bc4a0b0ed4c89a4e0c72
Devendra <devdangare88@gmail.com0>
2023-07-31 14:24:23 +0530

    changes

--- connection.js
+++ connection.js
@@ -1,22 +1,24 @@
-import mongoose,{connect} from "mongoose";
+import mongoose, { connect } from "mongoose";
 
 import dotenv from "dotenv";
 dotenv.config();
 
- async function Connect(){
-
-  console.log(process.env.DB_URL);
-    const ab = await mongoose.connect(process.env.DB_URL,{ 
+async function Connect()
+{
+        console.log(process.env.DB_URL);
+       
+       const ab= await mongoose.connect(process.env.DB_URL,{ 
         useUnifiedTopology: true, // For Mongoose 5 only. Remove for Mongoose 6+
         serverSelectionTimeoutMS: 1000, // Defaults to 30000 (30 seconds)
       }).then(()=>{
-        console.log('DataBase is connected !');
-      }).catch((e)=>{
+            console.log("DataBase is connected !");  
+    }).catch((error)=>{
+         console.log(error);
+    })
 
-        console.log("faild to connect DataBase! "+e);
-      })
+
+    
 
 }
-
 
 export default Connect;
